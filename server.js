d726a5d3778d3bae1bb561317ca881fdf5110ea2
Daniel Kuntz <kuntzd99@gmail.com>
2022-06-10 16:02:39 -0500

    Created boiler plate for MERN app

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
